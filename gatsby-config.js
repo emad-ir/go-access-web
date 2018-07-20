6918a58358b2d1e58ae51a71c456f7781f86f8c4
emad <emad@infinitereality.co.za>
2018-07-20 09:21:28 +0200

    push project inot repo

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,9 @@
 module.exports = {
-  siteMetadata: {
-    title: 'Gatsby Default Starter',
-  },
-  plugins: ['gatsby-plugin-react-helmet'],
+    siteMetadata: {
+        title: 'Go Access',
+    },
+    plugins: [
+        'gatsby-plugin-react-helmet',
+        'gatsby-plugin-sass'
+    ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,25 @@
-import React from 'react'
-import Link from 'gatsby-link'
+import React, { Component } from 'react'
+import scrollToComponent from 'react-scroll-to-component';
 
-const IndexPage = () => (
-  <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </div>
-)
+import Banner from '../components/Banner'
+import Contact from '../components/Contact'
+import Footer from '../components/Footer'
+
+class IndexPage extends Component {
+    handleScrollEvent = () => {
+        scrollToComponent(this.refs.contact, {
+            offset: 1000,
+        });
+    }
+    render() {
+        return (
+            <div>
+                <Banner onScroll={this.handleScrollEvent} />
+                <Contact ref="contact" />
+                <Footer />
+            </div>
+        )
+    }
+}
 
 export default IndexPage
